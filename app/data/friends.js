c283baff9e934e0135241db72b9ea33c3871791a
Paige Herbst <PMoney@Paiges-MacBook-Pro.local>
2019-03-22 11:52:13 -0600

    lets work

---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+var friends = [
+    {
+      name: "Ahmed",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Gob",
+      photo: "https://vignette.wikia.nocookie.net/arresteddevelopment/images/b/ba/Season_5_Character_Posters_-_G.O.B_02.jpg/revision/latest?cb=20180606212944",
+      scores: [
+        "2",
+        "5",
+        "1",
+        "4",
+        "2",
+        "4",
+        "1",
+        "4",
+        "2",
+        "1"
+      ]
+    },
+    {
+      name: "Tobias",
+      photo: "https://vignette.wikia.nocookie.net/arresteddevelopment/images/6/60/Season_4_Poster_-_Tobias_F%C3%BCnke_01.jpg/revision/latest/scale-to-width-down/300?cb=20130521213519",
+      scores: [
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "2",
+        "4",
+        "2",
+        "4"
+      ]
+    },
+    {
+      name: "Michael",
+      photo: "https://cdn2us.denofgeek.com/sites/denofgeekus/files/jasonbateman_michael.png",
+      scores: [
+        "4",
+        "4",
+        "3",
+        "2",
+        "1",
+        "3",
+        "5",
+        "4",
+        "1",
+        "2"
+      ]
+    },
+    {
+      name: "George",
+      photo: "https://img.sharetv.com/shows/characters/large/arrested_development.george_bluth_sr.jpg",
+      scores: [
+        "2",
+        "5",
+        "3",
+        "1",
+        "3",
+        "4",
+        "3",
+        "2",
+        "4",
+        "3"
+      ]
+    },
+    {
+      name: "Buster",
+      photo: "https://vignette.wikia.nocookie.net/arresteddevelopment/images/4/42/Season_4_Poster_-_Buster_Bluth_01.jpg/revision/latest/scale-to-width-down/300?cb=20130521213532",
+      scores: [
+        "4",
+        "3",
+        "5",
+        "2",
+        "4",
+        "1",
+        "5",
+        "3",
+        "4",
+        "3"
+      ]
+    }
+  
+  ];
+  
+  
+  module.exports = friends;
+  
